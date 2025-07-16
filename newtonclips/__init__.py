040a87ababf9d5e91464ce6d01c7873b8111d316
epw <88172828@qq.com>
2025-07-16 17:48:09 +0800

    add renderer and export frames, fix triangle indices bug, upgrade to newton-physics nightly

--- newtonclips/__init__.py
+++ newtonclips/__init__.py
@@ -1,39 +1,42 @@
 import hashlib
 import json
 import os
-import uuid
 from pathlib import Path
 
 import newton
 import numpy as np
 import warp as wp
-from newton import Model
-from newton.core.model import Vec3, Quat
+from newton import Model, Mesh, State
+from newton.core.types import Vec3, Quat, Transform, AxisType, Axis
 from trimesh import Trimesh, geometry
 
 
 class ModelBuilder(newton.ModelBuilder):
     SAVE_DIR: str | os.PathLike | None = None
+    SCALE: float = 1.0
 
-    def __init__(self, up_vector=(0.0, 1.0, 0.0), gravity=-9.80665):
+    def __init__(self, up_vector: AxisType = Axis.Z, gravity: float = -9.81):
         super().__init__(up_vector, gravity)
 
-        if ModelBuilder.SAVE_DIR is None:
-            raise RuntimeError('Unset ModelBuilder.SAVE_DIR')
+        if self.SAVE_DIR is None:
+            raise RuntimeError('Unset SAVE_DIR')
 
-        self._save_dir = Path(ModelBuilder.SAVE_DIR)
-        self._save_json = self._save_dir / 'model.json'
+        self._save_dir = Path(self.SAVE_DIR)
         self._cache_dir = self._save_dir / '.cache'
+        self._model_json = self._save_dir / 'model.json'
+        self._frame_dir = self._save_dir / 'frames'
 
-        self.save_dict = {
-            'Uuid': uuid.uuid4().hex,
+        self._model_dict = {
+            'Sha1': '',
+            'Scale': self.SCALE,
             'ShapeMesh': [],
             'SoftMesh': [],
         }
 
-    def save(self):
-        os.makedirs(self._save_json.parent, exist_ok=True)
-        self._save_json.write_text(json.dumps(self.save_dict, indent=4), 'utf-8')
+        self._frames = []
+
+        self.sim_time = 0.0
+        self.delta_time = 0.0
 
     def cache(self, hash_data: bytes | str) -> str | bytes | None:
         if isinstance(hash_data, bytes):
@@ -52,43 +55,18 @@ class ModelBuilder(newton.ModelBuilder):
     def add_shape_mesh(
             self,
             body: int,
-            mesh: newton.Mesh = None,
-            pos: Vec3 = wp.vec3(0.0, 0.0, 0.0),
-            rot: Quat = wp.quat(0.0, 0.0, 0.0, 1.0),
-            scale: Vec3 = wp.vec3(1.0, 1.0, 1.0),
-            density: float = newton.ModelBuilder.default_shape_density,
-            ke: float = newton.ModelBuilder.default_shape_ke,
-            kd: float = newton.ModelBuilder.default_shape_kd,
-            kf: float = newton.ModelBuilder.default_shape_kf,
-            ka: float = newton.ModelBuilder.default_shape_ka,
-            mu: float = newton.ModelBuilder.default_shape_mu,
-            restitution: float = newton.ModelBuilder.default_shape_restitution,
-            is_solid: bool = True,
-            thickness: float = newton.ModelBuilder.default_shape_thickness,
-            has_ground_collision: bool = True,
-            has_shape_collision: bool = True,
-            collision_group: int = -1,
-            is_visible: bool = True,
+            xform: Transform | None = None,
+            mesh: Mesh | None = None,
+            scale: Vec3 | None = None,
+            cfg: newton.ModelBuilder.ShapeConfig | None = None,
+            key: str | None = None,
     ):
         super().add_shape_mesh(
             body=body,
-            pos=pos,
-            rot=rot,
             mesh=mesh,
             scale=scale,
-            density=density,
-            ke=ke,
-            kd=kd,
-            kf=kf,
-            ka=ka,
-            mu=mu,
-            restitution=restitution,
-            is_solid=is_solid,
-            thickness=thickness,
-            has_ground_collision=has_ground_collision,
-            has_shape_collision=has_shape_collision,
-            collision_group=collision_group,
-            is_visible=is_visible,
+            cfg=cfg,
+            key=key,
         )
 
         if self.shape_geo_type[-1] != newton.GEO_MESH:
@@ -97,6 +75,7 @@ class ModelBuilder(newton.ModelBuilder):
         mesh = Trimesh(
             np.array(self.shape_geo_src[-1].vertices).reshape(-1, 3),
             np.array(self.shape_geo_src[-1].indices).reshape(-1, 3),
+            process=False,
         )
         mesh.fix_normals()
 
@@ -107,7 +86,7 @@ class ModelBuilder(newton.ModelBuilder):
             face_angles=mesh.face_angles,
         )
 
-        self.save_dict['ShapeMesh'].append({
+        self._model_dict['ShapeMesh'].append({
             'Body': self.shape_body[-1],
             'Transform': tuple(self.shape_transform[-1]),
             'Scale': tuple(self.shape_geo_scale[-1]),
@@ -119,7 +98,7 @@ class ModelBuilder(newton.ModelBuilder):
 
     def update_shape_mesh(self, i: int = -1, VertexUVs: np.ndarray[(-1, 2), np.float32] | None = None):
         if VertexUVs is not None:
-            self.save_dict['ShapeMesh'][i].update({
+            self._model_dict['ShapeMesh'][i].update({
                 'VertexUVs': self.cache(np.array(VertexUVs, np.float32).reshape(-1, 2).flatten().tobytes()),
             })
 
@@ -131,17 +110,18 @@ class ModelBuilder(newton.ModelBuilder):
             rot: Quat = wp.quat(0.0, 0.0, 0.0, 1.0),
             scale: float = 1.0,
             vel: Vec3 = wp.vec3(0.0, 0.0, 0.0),
-            density: float = newton.ModelBuilder.default_shape_density,
+            density: float = 1.0,
             k_mu: float = 1.0e3,
             k_lambda: float = 1.0e3,
             k_damp: float = 0.0,
-            tri_ke: float = newton.ModelBuilder.default_tri_ke,
-            tri_ka: float = newton.ModelBuilder.default_tri_ka,
-            tri_kd: float = newton.ModelBuilder.default_tri_kd,
-            tri_drag: float = newton.ModelBuilder.default_tri_drag,
-            tri_lift: float = newton.ModelBuilder.default_tri_lift,
+            tri_ke: float = 100.0,
+            tri_ka: float = 100.0,
+            tri_kd: float = 10.0,
+            tri_drag: float = 0.0,
+            tri_lift: float = 0.0,
     ):
-        begin = len(self.particle_q)
+        vtx_begin = len(self.particle_q)
+        tri_begin = len(self.tri_indices)
 
         super().add_soft_mesh(
             pos=pos,
@@ -161,16 +141,16 @@ class ModelBuilder(newton.ModelBuilder):
             tri_lift=tri_lift,
         )
 
-        count = len(self.particle_q) - begin
+        vtx_count = len(self.particle_q) - vtx_begin
+        tri_count = len(self.tri_indices) - tri_begin
 
-        if count <= 0:
-            raise RuntimeError(f'Invalid begin {begin} count {count}')
-
-        a, b = begin, begin + count
+        if vtx_count <= 0:
+            raise RuntimeError(f'Invalid begin {vtx_begin} count {vtx_count}')
 
         mesh = Trimesh(
-            np.array(self.particle_q).reshape(-1, 3)[a:b],
-            np.array(self.tri_indices).reshape(-1, 3)[a:b],
+            np.array(self.particle_q).reshape(-1, 3)[vtx_begin:vtx_begin + vtx_count],
+            np.array(self.tri_indices).reshape(-1, 3)[tri_begin:tri_begin + tri_count] - vtx_begin,
+            process=False,
         )
         mesh.fix_normals()
 
@@ -181,9 +161,9 @@ class ModelBuilder(newton.ModelBuilder):
             face_angles=mesh.face_angles,
         )
 
-        self.save_dict['SoftMesh'].append({
-            'Begin': begin,
-            'Count': count,
+        self._model_dict['SoftMesh'].append({
+            'Begin': vtx_begin,
+            'Count': vtx_count,
             'Vertices': self.cache(mesh.vertices.flatten().astype(np.float32).tobytes()),
             'Indices': self.cache(mesh.faces.flatten().astype(np.int32).tobytes()),
             'VertexNormals': self.cache(vertex_normals.flatten().astype(np.float32).tobytes()),
@@ -192,10 +172,66 @@ class ModelBuilder(newton.ModelBuilder):
 
     def update_soft_mesh(self, i: int = -1, VertexUVs: np.ndarray[(-1, 2), np.float32] | None = None):
         if VertexUVs is not None:
-            self.save_dict['SoftMesh'][i].update({
+            self._model_dict['SoftMesh'][i].update({
                 'VertexUVs': self.cache(np.array(VertexUVs, np.float32).reshape(-1, 2).flatten().tobytes()),
             })
 
     def finalize(self, device=None, requires_grad=False) -> Model:
-        self.save()
+        json_str = json.dumps(self._model_dict, sort_keys=True, ensure_ascii=False)
+        self._model_dict['Sha1'] = hashlib.sha1(json_str.encode('utf-8')).hexdigest()
+        os.makedirs(self._model_json.parent, exist_ok=True)
+        self._model_json.write_text(json.dumps(self._model_dict, indent=4, ensure_ascii=False), 'utf-8')
+
         return super().finalize(device, requires_grad)
+
+    @property
+    def renderer(self):
+        return self
+
+    def begin_frame(self, sim_time: float):
+        self.delta_time = sim_time - self.sim_time
+        self.sim_time = sim_time
+
+    def render(self, state: State):
+        body_q = state.body_q.numpy() if state.body_q is not None else []
+        particle_q = state.particle_q.numpy() if state.particle_q is not None else []
+
+        frame = {
+            'DeltaTime': self.delta_time,
+            'BodyTransform': self.cache(np.array(body_q, np.float32).reshape(-1, 7).flatten().tobytes()),
+            'ParticlePosition': self.cache(np.array(particle_q, np.float32).reshape(-1, 3).flatten().tobytes()),
+        }
+
+        os.makedirs(self._frame_dir, exist_ok=True)
+
+        frame_json = self._frame_dir / f'{len(self._frames)}.json'
+        frame_json.write_text(json.dumps(frame, indent=4, ensure_ascii=False), 'utf-8')
+
+        self._frames.append(frame)
+
+    def end_frame(self):
+        """"""
+
+
+def tri_surface_from_tet(tetrahedrons: np.ndarray) -> np.ndarray:
+    # 生成所有四面体的四个面，每个面顶点排序
+    faces_indices = np.array([[0, 2, 1], [1, 2, 3], [0, 1, 3], [0, 3, 2]])
+    faces = tetrahedrons[:, faces_indices].reshape(-1, 3)
+    sorted_faces = np.sort(faces, axis=1)
+
+    # 按字典序排序所有面
+    sort_order = np.lexsort((sorted_faces[:, 2], sorted_faces[:, 1], sorted_faces[:, 0]))
+    sorted_faces = sorted_faces[sort_order]
+
+    # 确定各面组及其出现次数
+    diff = np.ones(len(sorted_faces), dtype=bool)
+    diff[1:] = np.any(sorted_faces[1:] != sorted_faces[:-1], axis=1)
+    group_starts = np.where(diff)[0]
+    group_ends = np.append(group_starts[1:], len(sorted_faces))
+    group_lengths = group_ends - group_starts
+
+    # 提取仅出现一次的面
+    surface_mask = (group_lengths == 1)
+    surface_faces = sorted_faces[group_starts[surface_mask]]
+
+    return surface_faces
